31db3c989c2f2a3c441de47019c66f32421553a9
Hrishikesh <hrishih7o5@gmail.com>
2018-10-07 19:13:07 -0500

    Added check to see if number of cmd inputs was exactly 2

--- main.py
+++ main.py
@@ -218,10 +218,14 @@ class MDP(object):
 
 
 def main():
+    if(len(sys.argv) != 3):
+        print('Please enter exactly two arguments: <input_file> <discount_factor>')
+        exit()
     disc_fact = float(sys.argv[2])	# discount factor used in calculations. grabbed from command line
     if(disc_fact < 0 or disc_fact > 1):
         print('That is not a valid discount factor!')
         exit()
+    
     mdp = MDP(disc_fact, sys.argv[1])
     mdp.find_optimal_policies()
     print(str(mdp))
